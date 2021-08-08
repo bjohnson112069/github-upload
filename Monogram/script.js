367002f0b423761db7edf24844cad7bd5f54cef3
Brian Johnson <johnson-brian@verizon.net>
2021-08-08 09:43:25 -0400

    adding monogram

--- Monogram/script.js
+++ Monogram/script.js
@@ -1,6 +1,37 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const hasDownArrow = document.getElementsByClassName('has-down-arrow')[0]
+const dropdownContent = document.getElementsByClassName('dropdown-content')[0]
+const hamburgerMenu = document.getElementsByClassName('hamburger__menu')[0]
+const navbarList = document.getElementsByClassName('navbar--list')[0]
+const cookieContainer = document.getElementsByClassName('cookie__container')[0]
+const buttonLearnMore = document.getElementsByClassName('btn-learn')[0]
+const buttonUnderstand = document.getElementsByClassName('btn-understand')[0]
+const buttonSubmit = document.getElementsByClassName('btn-submit')[0]
 
-toggleButton.addEventListener('click', () => {
-     navbarLinks.classList.toggle('active')
+hasDownArrow.addEventListener('click', () => {
+     dropdownContent.classList.toggle('active')
+     hasDownArrow.classList.toggle('active')
 })
+
+hamburgerMenu.addEventListener('click', () => {
+     navbarList.classList.toggle('active')
+})
+
+buttonLearnMore.addEventListener('click', () => {
+     cookieContainer.classList.toggle('active')
+})
+
+buttonUnderstand.addEventListener('click', () => {
+     cookieContainer.classList.toggle('active')
+})
+
+function manage(txt) {
+     // var bt = document.getElementById('btSubmit');
+     if (txt.value != '') {
+          buttonSubmit.disabled = false;
+          console.log("Button Enabled")
+     }
+     else {
+          buttonSubmit.disabled = true;
+          console.log("Button Disabled")
+     }
+ }
