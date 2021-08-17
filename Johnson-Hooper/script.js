b17b8604cabc9173ef9e545e8f522e96eb2fef36
bjohnson112069 <johnson-brian@verizon.net>
2021-08-17 09:32:58 -0400

    johnson hooper revision

--- Johnson-Hooper/script.js
+++ Johnson-Hooper/script.js
@@ -3,6 +3,10 @@ const announcementContainer = document.getElementsByClassName('announcement__con
 const menuOpen = document.getElementsByClassName('menu-open')[0]
 const menuClose = document.getElementsByClassName('menu-close')[0]
 const navbarOverlay = document.getElementsByClassName('navbar__overlay')[0]
+const chatOpen = document.getElementsByClassName('chat__open')[0]
+const chatClose = document.getElementsByClassName('chat__close')[0]
+const messageContainer = document.getElementsByClassName('message__container')[0]
+const buttonSubmit = document.getElementsByClassName('btn-submit')[0]
 
 announcementClose.addEventListener('click', () => {
      announcementContainer.classList.toggle('active')
@@ -11,6 +15,23 @@ announcementClose.addEventListener('click', () => {
 menuOpen.addEventListener('click', () => {
      navbarOverlay.classList.toggle('active')
 })
+
 menuClose.addEventListener('click', () => {
      navbarOverlay.classList.toggle('active')
+})
+
+chatOpen.addEventListener('click', () => {
+     chatOpen.classList.toggle('active')
+     chatClose.classList.toggle('active')
+     messageContainer.classList.toggle('active')
+})
+
+chatClose.addEventListener('click', () => {
+     chatClose.classList.toggle('active')
+     chatOpen.classList.toggle('active')
+     messageContainer.classList.toggle('active')
+})
+
+buttonSubmit.addEventListener('click', () => {
+     buttonSubmit.classList.toggle('active')
 })
