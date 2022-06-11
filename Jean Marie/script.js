07c065a913b2f8b38b251e189e5a3045398a9551
bjohnson112069 <johnson-brian@verizon.net>
2022-06-11 11:34:24 -0400

    adding Jean Marie Johnson web site and misc projects

--- Jean Marie/script.js
+++ Jean Marie/script.js
@@ -1,7 +1,10 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const nav = document.querySelector(".primary-navigation");
+const navLinks = nav.querySelectorAll("li");
+var activeLink = nav.querySelector(".active");
+const toHome = document.querySelectorAll(".to-home");
 
-// when someone clise the hambruger button
+// when someone cliks the hambruger button
 navToggle.addEventListener("click", () => {
      // if the menu is close, open it
      const visibility = nav.getAttribute("data-visible");
@@ -14,4 +17,34 @@ navToggle.addEventListener("click", () => {
           nav.setAttribute("data-visible", false);
           navToggle.setAttribute("aria-expanded", false);
      }
+});
+
+// when someone click on the navigation links
+navLinks.forEach(navLink => {
+     navLink.addEventListener("click", () => {
+
+          // toggle old active link
+          activeLink.classList.toggle('active');
+          // toggle new active link
+          navLink.classList.toggle('active');
+          activeLink = navLink;
+          
+          // if the menu is open, close it
+          nav.setAttribute("data-visible", false);
+          navToggle.setAttribute("aria-expanded", false);
+     });          
+});
+
+// when someone clicks on the up/home section title link
+toHome.forEach(link => {
+     link.addEventListener("click", () => {
+          const firstLink = navLinks[0];
+          
+          // toggle old active link
+          activeLink.classList.toggle('active');
+          // toggle to first link in the nodelist (e.g. Home)
+          firstLink.classList.toggle('active');
+          activeLink = firstLink;
+
+     });
 });
