a73e56290f33dca4998c05ef7e939c98a2bc352e
bjohnson112069 <johnson-brian@verizon.net>
2024-12-11 11:12:05 -0500

    Backup Sync 12-11-2024

--- chat-app-css-illustration-master/script.js
+++ chat-app-css-illustration-master/script.js
@@ -1,9 +1,34 @@
 function loadContent() {
     // Configuration parameters
+    const tl = gsap.timeline();
+
+    function runAnimation() {
+        tl.to('.chat-app', {
+            y: 0,
+        })
+        tl.from('.hero-text > h1', {
+            y: -50,
+            opacity: 0
+        })
+        tl.from('.hero-text > p', {
+            y: 50,
+            opacity : 0
+        })
+        tl.to('.message', {
+            x: 0,
+            stagger: 0.3
+        });    
+    }
 
     // On page load... 
 
+    runAnimation();
+
+
     // Event Listeners
+    window.addEventListener('resize', () => {
+        tl.restart();
+    })
 }
 
 window.addEventListener('DOMContentLoaded', loadContent);
